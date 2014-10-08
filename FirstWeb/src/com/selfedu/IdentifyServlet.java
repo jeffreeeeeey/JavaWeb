e592ecbb72146fed667b34330b66742019d4c9ac
BM <jeffreeeeeey@gmail.com>
2014-10-08 21:04:35 +0800

    making the pic

--- FirstWeb/src/com/selfedu/IdentifyServlet.java
+++ FirstWeb/src/com/selfedu/IdentifyServlet.java
@@ -1,14 +1,20 @@
 package com.selfedu;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 public class IdentifyServlet extends HttpServlet {
 
@@ -27,6 +33,10 @@ public class IdentifyServlet extends HttpServlet {
 			stringBuffer.append(CHARS[random.nextInt(CHARS.length)]);
 		}
 		return stringBuffer.toString();
+	}
+	
+	public static Color getRandomColor(){
+		return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
 	}
 	
 	public static Color getReverseColor(Color c){
@@ -57,6 +67,29 @@ public class IdentifyServlet extends HttpServlet {
 		response.setContentType("image/jpeg");
 		String randomsString = getRandomString();
 		request.getSession(true).setAttribute("randomsString", randomsString);
+		
+		int width = 100;
+		int height = 30;
+		
+		Color color = getRandomColor();
+		Color reverseColor = getReverseColor(color);
+		BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		Graphics2D graphics2d = bufferedImage.createGraphics();
+		graphics2d.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
+		graphics2d.setColor(color);
+		graphics2d.fillRect(0, 0, width, height);
+		graphics2d.setColor(reverseColor);
+		graphics2d.drawString(randomsString, 18, 20);
+		
+		for (int i = 0, n = random.nextInt(100); i < n; i++) {
+			graphics2d.drawRect(random.nextInt(width), random.nextInt(height), 1, 1);
+		}
+		
+		ServletOutputStream outputStream = response.getOutputStream();
+		JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(outputStream);
+		
+		encoder.encode(bufferedImage);
+		outputStream.flush();
 	}
 
 	/**
@@ -94,6 +127,18 @@ public class IdentifyServlet extends HttpServlet {
 	 */
 	public void init() throws ServletException {
 		// Put your code here
+		
+	}
+	
+	private void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("</script>");
+		out.print("good");
 	}
 
 }
